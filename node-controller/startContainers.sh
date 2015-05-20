eea9d0b640a0d781f3d6c6247243c73d0f6790ce
ond9 <you@example.com>
2015-05-20 01:07:00 -0700

    ada

--- node-controller/startContainers.sh
+++ node-controller/startContainers.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -d --name node-controller node/node-controller
+docker run -d --name node-controller --privileged -v /var/run/docker.sock:/var/run/docker.sock node/node-controller
